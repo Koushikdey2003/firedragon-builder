48f78d0e810a5ff5537d7557bb333a84f052b00f
Stefan Wimmer <info@stefanwimmer128.xyz>
2025-05-21 22:01:57 +0200

    refactor(release): Only read version from package.json

--- release.ts
+++ release.ts
@@ -3,27 +3,15 @@
 import process from 'node:process';
 import packageJson from "./package.json" with { type: "json" };
 
-const argv = minimist(process.argv.slice(4), {
-    string: ['v', 'r'],
-})
+const version = `v${packageJson.version}-${packageJson.runtime}`;
 
-const current = packageJson.version.split('-');
-
-const version = argv.v ?? current[0];
-const release = argv.r ?? (argv.v ? 1 : current[1]);
-
-const full = `${version}-${release}`;
-const tag = `v${full}`;
-
-if (await $`git tag -l ${tag}`.text()) {
-    echo(`Tag ${tag} already exists. Aborting...`);
+if (await $`git tag -l ${version}`.text()) {
+    echo(`Tag ${version} already exists. Aborting...`);
     process.exit(1);
 }
 
-await $`jq '.version = '${JSON.stringify(full)} package.json | sponge package.json`;
-
-await $`git-cliff -u -t ${full} -p CHANGELOG.md`;
+await $`git-cliff -u -t ${version} -p CHANGELOG.md`;
 
 await $`git add package.json CHANGELOG.md`;
-await $`git commit -m 'release: '${tag}`;
-await $`git tag -m ${tag} ${tag}`
+await $`git commit -m 'release: '${version}`;
+await $`git tag -m ${version} ${version}`
