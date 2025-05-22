8a2c3639e2e2bab8cf9b6469e3824174cd067c0d
Stefan Wimmer <info@stefanwimmer128.xyz>
2025-05-22 19:54:03 +0200

    feat(release): Make release script update metainfo

--- release.ts
+++ release.ts
@@ -2,16 +2,30 @@
 
 import process from 'node:process';
 import packageJson from "./package.json" with { type: "json" };
+import { readFile, writeFile } from "node:fs/promises";
 
-const version = `v${packageJson.version}-${packageJson.runtime}`;
+const { version } = packageJson;
+
+async function updateMetainfo(file: string) {
+    const date = new Date();
+    const release = `
+    <release version="${version}" date="${date.getFullYear()}-${date.getMonth().toString().padStart(2, '0')}-${date.getDay().toString().padStart(2, '0')}">
+      <url type="details">https://gitlab.com/garuda-linux/firedragon/builder/-/releases/${version}</url>
+      <description />
+    </release>`;
+
+    await writeFile(file, (await readFile(file, 'utf-8')).replace('<releases>', `<releases>${release}`), 'utf-8');
+}
 
 if (await $`git tag -l ${version}`.text()) {
     echo(`Tag ${version} already exists. Aborting...`);
     process.exit(1);
 }
 
+await updateMetainfo('assets/org.garudalinux.firedragon.metainfo.xml');
+
 await $`git-cliff -u -t ${version} -p CHANGELOG.md`;
 
-await $`git add package.json CHANGELOG.md`;
+await $`git add package.json CHANGELOG.md assets/org.garudalinux.firedragon.metainfo.xml`;
 await $`git commit -m 'release: '${version}`;
 await $`git tag -m ${version} ${version}`
