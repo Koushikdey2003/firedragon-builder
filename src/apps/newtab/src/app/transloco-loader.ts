9f0a4a17ca588ae60b17fdf422d57e58c856d9e2
Stefan Wimmer <info@stefanwimmer128.xyz>
2025-06-15 16:47:53 +0200

    fix(newtab): Load translations relative to base href

--- src/apps/newtab/src/app/transloco-loader.ts
+++ src/apps/newtab/src/app/transloco-loader.ts
@@ -1,14 +1,12 @@
-import { Injectable } from "@angular/core";
+import { Injectable, inject } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import type { Translation, TranslocoLoader } from "@jsverse/transloco";
-import { of } from "rxjs";
 
 @Injectable({ providedIn: "root" })
 export class TranslocoHttpLoader implements TranslocoLoader {
-  // private http = inject(HttpClient);
+  private http = inject(HttpClient);
 
   getTranslation(lang: string) {
-    return of({});
-    // return this.http.get<Translation>(`/src/public/i18n/${lang}.json`);
+    return this.http.get<Translation>(`i18n/${lang}.json`);
   }
 }
