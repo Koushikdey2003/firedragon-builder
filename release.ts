fe996966c14c34e10c6b57251c3355dc25ce789b
Stefan Wimmer <info@stefanwimmer128.xyz>
2025-06-18 12:54:32 +0200

    refactor(release): Use xml-js to update metainfo instead of string replace

--- release.ts
+++ release.ts
@@ -1,20 +1,29 @@
 /// <reference types="zx/globals" />
 
+import { readFile, writeFile } from 'node:fs/promises';
 import process from 'node:process';
+import { ElementCompact, js2xml, xml2js } from "npm:xml-js";
 import packageJson from "./package.json" with { type: "json" };
-import { readFile, writeFile } from "node:fs/promises";
 
 const version = `v${packageJson.version}`;
 
 async function updateMetainfo(file: string) {
+    const metainfo: ElementCompact = xml2js(await readFile(file, 'utf-8'), { compact: true });
     const date = new Date();
-    const release = `
-    <release version="${version}" date="${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}">
-      <url type="details">https://gitlab.com/garuda-linux/firedragon12/builder/-/releases/${version}</url>
-      <description />
-    </release>`;
-
-    await writeFile(file, (await readFile(file, 'utf-8')).replace('<releases>', `<releases>${release}`), 'utf-8');
+    metainfo.component.releases.release.unshift({
+        _attributes: {
+            version,
+            date: `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`,
+        },
+        url: {
+            _attributes: {
+                type: 'details',
+                _text: `https://gitlab.com/garuda-linux/firedragon12/builder/-/releases/${version}`,
+            },
+            description: {},
+        },
+    });
+    await writeFile(file, js2xml(metainfo, { compact: true, spaces: 2 }), 'utf-8');
 }
 
 if (await $`git tag -l ${version}`.text()) {
